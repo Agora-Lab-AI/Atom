052b6b99a53837b216a811b21f0f9563252455c4
Kye <kye@apacmediasolutions.com>
2023-10-05 14:58:48 -0400

    mistral finetune

---
+++ mistral.py
@@ -0,0 +1,174 @@
+import os
+from datetime import datetime
+
+import torch
+import transformers
+import wandb
+from accelerate import Accelerator, FullyShardedDataParallelPlugin
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from torch.distributed.fsdp.fully_sharded_data_parallel import (
+    FullOptimStateDictConfig,
+    FullStateDictConfig,
+)
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+)
+
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
+
+
+#
+train_dataset = load_dataset('kye/metamath-mistal-tokenized-16384') #split='train')
+eval_dataset = load_dataset('kye/metamath-mistal-tokenized-16384')#split='validation')
+test_dataset = load_dataset('kye/metamath-mistal-tokenized-16384')#split='test')
+
+
+base_model_id = "mistralai/Mistral-7B-v0.1"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(base_model_id, quantization_config=bnb_config)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    model_max_length=512,
+    padding_side="left",
+    add_eos_token=True)
+
+tokenizer.pad_token = tokenizer.eos_token
+
+def tokenize(prompt):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=512,
+        padding="max_length",
+    )
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+def generate_and_tokenize_prompt(data_point):
+    full_prompt =f"""Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+    This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+    The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+    ### Target sentence:
+    {data_point["target"]}
+
+    ### Meaning representation:
+    {data_point["meaning_representation"]}
+    """
+    return tokenize(full_prompt)
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = eval_dataset.map(generate_and_tokenize_prompt)
+
+
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+
+config = LoraConfig(
+    r=8,
+    lora_alpha=16,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+        "lm_head",
+    ],
+    bias="none",
+    lora_dropout=0.05,  # Conventional
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+# Apply the accelerator. You can comment this out to remove the accelerator.
+model = accelerator.prepare_model(model)
+
+
+wandb.login()
+
+wandb_project = "mistral-finetune"
+if len(wandb_project) > 0:
+    os.environ["WANDB_PROJECT"] = wandb_project
+
+
+if torch.cuda.device_count() > 1: # If more than 1 GPU
+    model.is_parallelizable = True
+    model.model_parallel = True
+
+
+project = "mistral-finetune"
+base_model_name = "mistral"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+tokenizer.pad_token = tokenizer.eos_token
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=train_dataset,
+    args=transformers.TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=5,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        max_steps=1000,
+        learning_rate=2.5e-5, # Want about 10x smaller than the Mistral learning rate
+        logging_steps=50,
+        bf16=True,
+        optim="paged_adamw_8bit",
+        logging_dir="./logs",        # Directory for storing logs
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=50,                # Save checkpoints every 50 steps
+        evaluation_strategy="steps", # Evaluate the model every logging step
+        eval_steps=50,               # Evaluate and save checkpoints every 50 steps
+        do_eval=True,                # Perform evaluation at the end of training
+        report_to="wandb",           # Comment this out if you don't want to use weights & baises
+        run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
+
+
+# base_model = AutoModelForCausalLM.from_pretrained(
+#     base_model_id,  # Mistral, same as before
+#     quantization_config=bnb_config,  # Same quantization config as before
+#     device_map="auto",
+#     trust_remote_code=True,
+#     use_auth_token=True
+# )
+
+# tokenizer = AutoTokenizer.from_pretrained(base_model_id, trust_remote_code=True)
+# tokenizer.pad_token = tokenizer.eos_token

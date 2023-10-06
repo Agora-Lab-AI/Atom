d78afba2097e6056e69072553d7567f9d976749f
Kye <kye@apacmediasolutions.com>
2023-10-06 11:00:17 -0400

    clean up

--- run.sh
+++ run.sh
@@ -13,6 +13,6 @@ accelerate launch finetune.py \
     --learning-rate 2e-5 \
     --grad-norm \
     --lora \
-    --model kye/atom-65k-pytorch \
+    --model conceptofmind/Yarn-Llama-2-13b-64k \
     --yarn-factor 16.0 \
     --dataset kye/all-lucidrain-code-python-tokenized-65536-1
